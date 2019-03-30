3c40d3236058c9f4ad35794bdfd779cbc16d70c4
Lucas <lucastakahashi@usp.br>
2019-03-30 14:10:03 -0300

    git inicial

---
+++ ola.js
@@ -0,0 +1,39 @@
+function formValidator( e ){
+    let saida = document.getElementById("saida");
+    let texto = e.Name.value;
+    let textoSaida ="";
+    const lim = e.qtdCaracter.value;
+    let conta = 0;
+    let funcName = e.txtFuncao.value;
+    let openFunction = e.checkbox.checked == true ? "(" : "";
+    let closeFunction = e.checkbox.checked == true ? ")" : "";
+    openFunction += e.sdelimiter.value;
+    closeFunction = e.sdelimiter.value + closeFunction + ( e.chbPonto.checked == true ? ";" : "");
+    saida.innerHTML = "";
+    textoSaida += "<p>" + funcName + openFunction + "";
+    for(var i = 0 ; i < texto.length ; i++){
+        if(conta == lim){
+            textoSaida += closeFunction + ";</p><p>" + funcName + openFunction ;
+            conta = 0;
+        }
+        if(texto.charAt(i) == '"'){
+            textoSaida += "\\\"";
+        }else if(texto.charAt(i) == '\n'){
+            textoSaida += "\\n";
+        }else if(texto.charAt(i) == '\''){
+            textoSaida += "\\'";
+        }else if(texto.charAt(i) == '<'){
+            textoSaida += "&lt;";
+        }else if(texto.charAt(i) == '>'){
+            textoSaida += "&gt;";
+        }else{
+            textoSaida += texto.charAt(i);
+        }
+        conta++;
+    }
+    textoSaida+=closeFunction;
+    console.log(textoSaida);
+    saida.innerHTML = textoSaida;
+    return false;
+}
+
